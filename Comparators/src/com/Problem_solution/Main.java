e7d1d74e0be88f33ad37fd7536a93e18fb1c1e8f
Fahmida Akter <fahmida.akm3@gmail.com>
2021-11-16 16:07:26 +0600

    Final Commit

---
+++ Comparators/src/com/Problem_solution/Main.java
@@ -0,0 +1,27 @@
+package com.Problem_solution;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int testCases = Integer.parseInt(in.nextLine());
+
+        List<Student> studentList = new ArrayList<Student>();
+        while(testCases>0){
+            int id = in.nextInt();
+            String fname = in.next();
+            double cgpa = in.nextDouble();
+
+            Student st = new Student(id, fname, cgpa);
+            studentList.add(st);
+
+            testCases--;
+        }
+        Collections.sort(studentList,  Comparator.comparing(Student :: getCgpa).reversed().thenComparing(Student :: getFname).thenComparing(Student :: getId));
+        for(Student st: studentList){
+            System.out.println(st.getFname());
+        }
+    }
+}
